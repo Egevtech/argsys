40f833e73b58390d3d6f8b3a45581e6f0d571576
Pavel Suvalov <egevtech@gmail.com>
2025-05-22 17:46:51 +0300

    Preparation

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ pub struct Arg
 {
     pub template: String,
     pub short: Option<String>,
-    pub descript: String
+    pub descript: String,
+    pub param_num: usize
 }
 
 pub fn build_arg( template: &str, short: Option<&str>, descript: &str ) -> Arg 
@@ -12,8 +13,8 @@ pub fn build_arg( template: &str, short: Option<&str>, descript: &str ) -> Arg
 
         template: template.to_string(),
         short: if short == None { None } else { Some(short.unwrap().to_string()) },
-        descript: descript.to_string()
-
+        descript: descript.to_string(),
+        param_num: 0
     }
 }
 
