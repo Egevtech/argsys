38c6f31613ab25210f889d242287110976880cb8
Pavel Suvalov <egevtech@gmail.com>
2025-05-22 17:43:21 +0300

    A bit of beauty

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,9 @@ pub fn get_arg_index( args: Vec<Arg>, template: String ) -> Option<usize>
 
     for i in  0..args.len() {
         let current = args[i].clone();
-        if [current.template.clone(), if current.short.clone() == None { "".to_string() } else { current.short.unwrap().clone() }].contains(&template.clone()) {
+        if [current.template.clone(),
+            if current.short.clone() == None { "".to_string() } else
+                { current.short.unwrap().clone() }].contains(&template.clone()) {
             return Some(i);
         }
     }
