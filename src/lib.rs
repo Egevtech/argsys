a1b4a586fd9c9fe743005a275e6a0d22b5ca3f38
Pavel Suvalov <egevtech@gmail.com>
2025-05-24 13:09:38 +0300

    Removed unused newline

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@ pub struct Arg
 pub fn build_arg( template: &str, short: Option<&str>, descript: &str, param_num: usize ) -> Arg 
 {
     Arg {
-
         template: template.to_string(),
         short: if short == None { None } else { Some(short.unwrap().to_string()) },
         descript: descript.to_string(),
